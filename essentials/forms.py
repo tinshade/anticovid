b887ec3299bd949f534a8c9e620d741af5378797
tinshade <tinshade98@gmail.com>
2020-04-27 12:10:16 +0530

    Added the essentials app with resources

--- essentials/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class EssentialsForm(forms.Form):
-    StateName = forms.CharField(label='State', max_length=80)
-    CityName = forms.CharField(label='City', max_length=80)
-    ServiceName = forms.CharField(label='Service', max_length=200)
