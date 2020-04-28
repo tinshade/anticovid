d9a9d58a7a1645e607c7d6cc67c8382fee82c560
tinshade <tinshade98@gmail.com>
2020-04-28 13:42:14 +0530

    Major Bug Fixes

--- main/static/knowledge.js
+++ main/static/knowledge.js
@@ -1,93 +1,105 @@
-function disclaimer() {
-  var x = document.getElementById("dd");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function getstates(){
+  var url = 'https://api.covid19india.org/resources/resources.json';
+  var states = []
+  var statenames = document.getElementById('statenames')
+  fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(myJson){
+    statenames.innerHTML = `<option value=""></option>`
+    myJson.resources.forEach(myprinter);
+    function myprinter(item){
+      if(states.includes(item.state) == false){
+        states.push(item.state)
+      }
+    }
+    for(var i=0; i<states.length; i++){
+      statenames.innerHTML += `<option value="${states[i]}">${states[i]}</option>`
+    }
+  })
 }
-
-var wait = document.getElementById('wait')
-var notfoundre =  document.getElementById('notfoundre')
-
-function getme(){
-	document.getElementById('tabres').innerHTML = ""  
-  wait.style.display = "block"
-	var url = 'https://api.covid19india.org/resources/resources.json';
-  	fetch(url)
-    .then(function(response) {
+function getcica(){
+  var url = 'https://api.covid19india.org/resources/resources.json';
+  var cities = []
+  var services = []
+  var statename = document.getElementById('statenames').value
+  var citiname = document.getElementById('citynames')
+  var categories = document.getElementById('categories')
+  fetch(url)
+  .then(function(response) {
       return response.json();
     })
     .then(function(myJson) {
+      citiname.innerHTML = ``
+      categories.innerHTML = ``
+      myJson.resources.forEach(myprinter);
+      function myprinter(item){
+        if(item.state == statename){
+          if(cities.includes(item.city) == false){
+            cities.push(item.city)
+          }
+          if(services.includes(item.category) == false){
+            services.push(item.category)
+          }
+        }
+      }
+      for(var i = 0; i<=cities.length-1; i++){
+        citiname.innerHTML += `<option value="${cities[i]}">${cities[i]}</option>`  
+      }
+      for(var i = 0; i<=services.length-1; i++){
+        categories.innerHTML += `<option value="${services[i]}">${services[i]}</option>`  
+      }
+    })
+}
+
+function getser(){
+  var url = 'https://api.covid19india.org/resources/resources.json';
+  var services = []
+  var citiname = document.getElementById('citynames').value
+  var categories = document.getElementById('categories')
+  fetch(url)
+  .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      categories.innerHTML = ``
+      myJson.resources.forEach(myprinter);
+      function myprinter(item){
+        if(item.city == citiname){
+          if(services.includes(item.category) == false){
+            services.push(item.category)
+          }
+        }
+      }
+      for(var i = 0; i<=services.length-1; i++){
+        categories.innerHTML += `<option value="${services[i]}">${services[i]}</option>`  
+      }
+    })
+}
+
+function getall(){
+  var url = 'https://api.covid19india.org/resources/resources.json';
+  var statename = document.getElementById('statenames').value 
+  var citiname = document.getElementById('citynames').value
+  var categories = document.getElementById('categories').value
+  var wait = document.getElementById('wait')
+  var main = document.getElementById('tabmain')
+  wait.style.display = "block"
+  main.style.display = "none"
+  fetch(url)
+  .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      document.getElementById('tabres').innerHTML = ``
       myJson.resources.forEach(myprinter);
       wait.style.display = "none"
-      notfoundre.style.display = "none"
-      document.getElementById("tabmain").style.display = "block"
+      main.style.display = "block"
       function myprinter(item){
-      	var stinp = document.getElementById('sts').value
-      	var ciinp1 = document.getElementById('state').value
-        var ciinp = ciinp1.trim()
-	      var catinp = document.getElementById('service').value
-	      var tabres = document.getElementById('tabres')
-        if(stinp == ""){
-          stinp = "ALL"
+        if(item.state == statename && item.city == citiname && item.category == categories){
+          tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td>${item.nameoftheorganisation}</td><td>${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
         }
-        if(ciinp == ""){
-          ciinp = "ALL"
-        }
-        if(catinp == ""){
-          catinp = "ALL"
-        }
-        if(stinp == "ALL" && ciinp == "ALL" && catinp != "ALL"){
-          if(item.state == stinp && item.category == catinp ){
-            tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-          }else{
-            tabres.innerHTML =`<h1>No Results</h1>`
-          }          
-        }else if(stinp == "ALL" && ciinp != "ALL" && catinp != "ALL"){
-          if(item.state == stinp && item.city == ciinp && item.category == catinp ){
-            tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-          }else{
-            tabres.innerHTML =`<h1>No Results</h1>`
-          }
-        }
-        if(stinp !="ALL"){
-         if(ciinp == "ALL" && catinp == "ALL" ){
-            if (item.state == stinp){
-              tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-            }
-          }else if(ciinp !="ALL" && catinp == "ALL"){
-            if(ciinp != item.city){
-              document.getElementById("tabmain").style.display = "none"
-              notfoundre = "block"
-              return
-            }else{
-              if(item.city == ciinp){
-                tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-              }
-            }
-          }else if(ciinp !="ALL" && catinp != "ALL"){
-            if(item.city == ciinp){
-              if(item.category == catinp){
-                tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-              }
-            }else{
-              tabres.innerHTML = `<h1>NO RESULTS</h1>`
-            }
-          }else if(ciinp == "ALL" && catinp != "ALL"){
-            if(item.category == catinp){
-              if(item.state == stinp && item.category == catinp){
-                tabres.innerHTML +=`<tr><td>${item.city}</td><td>${item.category}</td><td class="org">${item.nameoftheorganisation}</td><td class="des">${item.descriptionandorserviceprovided}</td><td>${item.phonenumber}</td></tr>`
-              }
-            }else{
-              document.getElementById("tabmain").style.display = "none"
-              document.getElementById('notfoundre').style.display = "block"
-            }
-            
-          }
-        }
-      	
       }
-    
-  });
+    })
 }
